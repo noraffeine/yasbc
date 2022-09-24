33c797262d3df7a914545ec0b753d4d3a825c260
noraffeine <88846773+noraffeine@users.noreply.github.com>
2022-09-24 02:10:26 +0100

    check if the letters are in the words

--- back.py
+++ back.py
@@ -1,8 +1,26 @@
 from utils.read_write_json import read_json, write_json
-import itertools
+from itertools import combinations
+import numpy as np
 import string
 
 words = read_json('./word_list/words.json')
 
-letters = string.ascii_lowercase.split('')
-print(letters)
+letters = [i for i in string.ascii_lowercase]
+
+combos = list(combinations(letters, 3))
+test = [0, 2, 3]
+
+
+def make_array():
+    arr = np.zeros((len(letters),len(words)))
+    for idx1,c in enumerate(letters):
+        for idx2,w in enumerate(words):
+            if c in w:
+                arr[idx1][idx2] = 1
+    return arr
+
+M = make_array()
+F = (M[test][:]).sum(axis=0).flatten()
+G = np.argwhere(F>2).flatten()
+print(G)
+
