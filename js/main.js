e6bb44618cf690725129f27e8319ded85c21bcc6
noraffeine <88846773+noraffeine@users.noreply.github.com>
2022-09-26 03:05:55 +0100

    points and progress bar now updates

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     var letters = "efmu"; 
     
     var words = ["merciful", "perfumed", "farmhouse", "muffled", "furthermore", "motherfucker", "manufacturers", "magnifique", "manufactured", "fume", "masterful", "dumbfounded", "malfunctioned", "meaningful", "perfumes", "fumes", "manufacturer", "muffler", "fundamentals", "formulate", "misfortunes", "uniformed", "remorseful", "foursome", "shamefully", "fumble", "unfathomable", "unconfirmed", "referendum", "formulated", "mercifully", "camouflage", "mummified", "uncomfortable", "fundamentally", "fumigate", "fulfilment", "fundamental", "shameful", "uninformed", "fundamentalists", "perfume", "manufactures", "femur", "misfortune", "fumigated", "muffle", "fulfilment", "circumference", "fumbled", "camouflaged", "fundamentalist", "manufacture"];
-    var currentword = '';
+    var currentWordArr = [];
     var availablePoints = 0;
+    var keyBox = document.getElementById("key-box")
+    var pointsDisplay = document.getElementById("points-text")
+    var progressBar = document.getElementById("bar-show")
+    var numWordsFound = document.getElementById("words-found")
     var gottenPoints = 0;
     var foundWords = [];
     var pointsMap = {};
@@ -21,6 +25,74 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     createSquares(letters);
+    window.addEventListener("keyup", checkKeyPressed, false);
+
+    function checkKeyPressed(evt) {
+        if (evt.keyCode == "13") {
+            handleSubmitWord();
+            return;
+        }
+        if (evt.keyCode == "8") {
+            handleDeleteLetter();
+            return;
+        }
+        else{
+            char = String.fromCharCode(evt.keyCode).toLowerCase()
+            if(/[a-z]/.test(char)){
+                updateCurrentWord(char)
+            }
+        }
+    }
+    function updateCurrentWordDisplay(){
+        let currentWord = currentWordArr.join('')
+        keyBox.textContent = currentWord;
+    }
+
+    function updatePoints(word){
+        gottenPoints = gottenPoints + pointsMap[word]
+
+        pointsDisplay.textContent = gottenPoints;
+        if (foundWords.length == 1){
+            numWordsFound.textContent = `You have found ${foundWords.length} word`
+        }
+        if (foundWords.length > 1){
+            numWordsFound.textContent = `You have found ${foundWords.length} words`
+        }
+        
+        let progress = (gottenPoints/availablePoints)*100;
+        console.log(progress)
+        progressBar.style.width = `${progress}%`
+        console.log(progressBar.style)
+    }
+
+    function updateCurrentWord(char){
+        currentWordArr.push(char);
+        updateCurrentWordDisplay();
+    }
+
+    function handleDeleteLetter(){
+        if(currentWordArr.length >0){
+            currentWordArr.pop()
+            updateCurrentWordDisplay();
+        }
+    }
+
+    function handleSubmitWord(){
+        let currentWord = currentWordArr.join('')
+        currentWordArr = []; 
+        if(words.includes(currentWord)){
+            console.log("correct")
+            foundWords.push(currentWord)
+            updateCurrentWordDisplay();
+            updatePoints(currentWord);
+
+        }
+        }
+    
+    function checkWord(){
+
+    }
+
     function createSquares(letters){
         const gameBoard = document.getElementById("letter-board")
         for (var i = 0; i < letters.length; i++){
